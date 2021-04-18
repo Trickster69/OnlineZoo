529917fe3a238403cc98755f7847a85eea8cc05a
Sergei Demchuk <vinci.ru@mail.ru>
2021-04-18 16:12:26 +0300

    feat: add adaptive map page

--- online-zoo/pages/landing/main.js
+++ online-zoo/pages/landing/main.js
@@ -1,7 +1,9 @@
 const burger = document.querySelector('.burger'),
-    mobileNav = document.querySelector('.header-mobile__navigation');
+    mobileNav = document.querySelector('.header-mobile__navigation'),
+    mobileHeader = document.querySelector('.header-mobile');
 
     burger.addEventListener('click', ()=> {
         console.log("sdfsfsd");
         mobileNav.classList.toggle('header-mobile-nav_show');
+        mobileHeader.classList.toggle('header-mobile_back');
     });
