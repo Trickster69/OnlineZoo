fa7c38c92cb9f6f5793887657e6706d477664b8e
Sergei Demchuk <vinci.ru@mail.ru>
2021-04-28 19:08:15 +0300

    feat: add js to map markers

--- online-zoo/pages/landing/main.js
+++ online-zoo/pages/landing/main.js
@@ -4,68 +4,177 @@ const burger = document.querySelector('.burger'),
     overlay = document.querySelector('.overlay'),
     body = document.querySelector('body'),
     feedBackBtn = document.querySelector('.fifth-screen__button');
-
-    burger.addEventListener('click', ()=> {
+    
+    //Show-hidden mobile menu
+    function menuMobileToggle() {
         mobileNav.classList.toggle('header-mobile-nav_show');
         mobileHeader.classList.toggle('header-mobile_back');
+    }
+
+    //Show-hidden overlay ond overflow;
+    function overlayToggle() {
         overlay.classList.toggle('overlay_mobile');
         body.classList.toggle('body_scroll-block');
+    }
 
-        document.querySelector('.overlay_mobile').addEventListener('click', () => {
-            overlay.classList.remove('overlay_mobile');
-            mobileNav.classList.remove('header-mobile-nav_show');
-            mobileHeader.classList.remove('header-mobile_back');
-            body.classList.remove('body_scroll-block');
+    
+    burger.addEventListener('click', ()=> {
+        menuMobileToggle();
+        overlayToggle();
+    });
+
+    //click overlay
+    document.querySelector('.overlay').addEventListener('click', () => {
+        if(document.getElementById('checkbox').checked == true){
+            menuMobileToggle();
+            overlayToggle();            
             document.getElementById('checkbox').checked = false;
+        }
+       
+    });
+
+    
+    //**************        MODAL        *******************//
+    const modalFeedback = document.querySelector('.feedback'),
+        formFeedback = document.querySelector('#feedback__form'),
+        nameInput = document.querySelector('.feedback__name').querySelector('input'),
+        emailInput = document.querySelector('.feedback__email').querySelector('input'),
+        modalTextarea = document.querySelector('.feedback__review').querySelector('textarea'),
+        modalFields = document.querySelectorAll('#input'),
+        sendModalButton = document.querySelector('.feedback__send-button'),
+        feedbackButton = document.querySelector('.fifth-screen__button'),
+        mobileCloseBtn = document.querySelector('.feedback_close');
+
+    //Close modal function
+    function closeModal () {
+        overlay.classList.remove('overlay_mobile');
+        body.classList.remove('body_scroll-block');
+        document.querySelector('.feedback').classList.remove('feedback_show'); 
+        formFeedback.reset();
+        sendModalButton.style.background = '#D3D3D3';
+        sendModalButton.setAttribute('disabled', 'disabled');
+    }
+
+    //Check inputs function    
+    function checkInputValue() {
+        const textareaValue = modalTextarea.value;
+
+        if(nameInput.validity.valid && emailInput.validity.valid && (textareaValue.length != 0 && textareaValue.length < 280)) {
+            sendModalButton.removeAttribute('disabled');
+            sendModalButton.style.background = '#0F0049';
+        } else {
+            sendModalButton.style.background = '#D3D3D3';
+            sendModalButton.setAttribute('disabled', 'disabled');
+        }
+    }
+    modalFields.forEach(field => {
+        field.addEventListener('keyup', ()=> {
+            checkInputValue();
         });
     });
 
-    //MODAL Feedback
-    const nameFeedbackModal = document.querySelector('.feedback__name').querySelector('input'),
-    emailFeedbackModal =document.querySelector('.feedback__email').querySelector('input'),
-    textareaFeedbackModal =document.querySelector('.feedback__review').querySelector('textarea'),
-    buttonFeedbackModal = document.querySelector('.feedback__send-button');
-    var inputs = document.querySelectorAll('input[type = "email"]');
+    //Show modal
+    feedbackButton.addEventListener('click', () => {
+        modalFields.forEach(field => field.value = "");
+        modalFeedback.classList.add('feedback_show');
+        overlay.classList.add('overlay_mobile');
+        body.classList.add('body_scroll-block');
+    }); 
 
-    feedBackBtn.addEventListener('click', ()=> {
-        for (var i = 0;  i < inputs.length; i++) {
-            inputs[i].value = '';
+    //Close modal to click on overlay    
+    overlay.addEventListener('click', () => {
+        if(modalFeedback.classList.contains('feedback_show')){
+            closeModal();
         }
-        overlay.classList.toggle('overlay_mobile');
-        body.classList.toggle('body_scroll-block');
-        document.querySelector('.feedback').classList.toggle('feedback_show');
-        buttonFeedbackModal.setAttribute('disabled', true);
-        buttonFeedbackModal.style.background = '#D3D3D3';
+    });
 
-        nameFeedbackModal.addEventListener('click', ()=> {
-            console.log(nameFeedbackModal.value.length);
-        });
-        document.querySelector('.feedback').addEventListener('click', ()=> {
-            if(nameFeedbackModal.value.length > 0 && emailFeedbackModal.value.length > 0 &&(textareaFeedbackModal.value.length > 0 && textareaFeedbackModal.value.length < 5)){
-                
-                buttonFeedbackModal.removeAttribute('disabled');
-                buttonFeedbackModal.style.background = '#171717';
-                buttonFeedbackModal.style.color = '#FFFFFF';
+    //Close form to ESC    
+    document.addEventListener('keydown', (e)=> {
+        if (e.code === 'Escape' && modalFeedback.classList.contains('feedback_show')) {
+            closeModal();   
+        }
+    });
 
-                if(textareaFeedbackModal.value.length > 5) {
-                    buttonFeedbackModal.setAttribute('disabled', true);
-                    buttonFeedbackModal.style.background = '#D3D3D3';
-                }
+    //Close form to X 
+    mobileCloseBtn. addEventListener('click', ()=> {
+        closeModal();
+    });
+
+    //Send form
+    formFeedback.addEventListener('submit', (e)=> {
+        e.preventDefault();
+        alert(`Thanks for your feedback, ${nameInput.value}!`);
+        closeModal();
+    });
+
+
+    //**************       MAP       *******************//
+    const markers = document.querySelectorAll('.map-animal-icons__marker'),
+    map = document.querySelector('.sixth-screen__map'),
+    mapCards = document.querySelectorAll('.map-card'),
+    mobileAsideMap = document.querySelectorAll('.aside__animal-item');
+
+    function showMapCard(id){
+        mapCards.forEach(mapCard => {
+            if(mapCard.classList.contains(id)){
+                mapCards.forEach(Card => Card.classList.remove('show_map-card'))
+                mapCard.classList.add('show_map-card');
             }
         });
-        
-        
+    }
 
-        document.querySelector('.overlay_mobile').addEventListener('click', () => {
-            overlay.classList.remove('overlay_mobile');
-            mobileNav.classList.remove('header-mobile-nav_show');
-            mobileHeader.classList.remove('header-mobile_back');
-            body.classList.remove('body_scroll-block');     
-            document.querySelector('.feedback').classList.remove('feedback_show');
-            document.querySelector('.feedback__input').focus();
+    mobileAsideMap.forEach(mobileAsideElement => {
+        mobileAsideElement.addEventListener('click', (e)=> {
+            mobileAsideMap.forEach(elem => {
+                elem.classList.remove('acide_active'); 
+                elem.classList.add('acide_disable');
+            }); 
+
+            if(mobileAsideElement.id == e.target.id){
+                console.log('sdfsdf');
+                mobileAsideElement.classList.remove('acide_disable');
+                mobileAsideElement.classList.add('acide_active');
+            }
+            
+            showMapCard(e.target.id);
         });
     });
 
+    
+    map.addEventListener('click', (e) => {
+        if (e.target.classList.contains('map-animal-icon__border') || 
+        e.target.classList.contains('map-animal-icons__item') || 
+        e.target.classList.contains('map-animal-icons_piece')) {
+            
+            markers.forEach(marker => {
+                marker.classList.remove('marker_active');
+                let id = e.target.id;
+
+                if(marker.classList.contains(id)) {
+                    marker.classList.add('marker_active');
+                }
+                showMapCard(id);
+            });
+
+        } else {
+            markers.forEach(marker => {
+                marker.classList.remove('marker_active');
+            });            
+        }
+    });
+
+
+
+   
+
+
+
+    
+
+     
+        
+
+    
 
 
     
