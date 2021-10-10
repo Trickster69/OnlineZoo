429efd97f179c4283addfcc8bbbb65e3131e3961
Sergei Demchuk <vinci.ru@mail.ru>
2021-10-10 15:53:34 +0300

    fix: fix adresses

--- online-zoo/pages/zoos/panda/panda.js
+++ online-zoo/pages/zoos/panda/panda.js
@@ -2,6 +2,5 @@ const burger = document.querySelector('.burger'),
     mobileNav = document.querySelector('.header-mobile__navigation');
 
     burger.addEventListener('click', ()=> {
-        console.log("sdfsfsd");
         mobileNav.classList.toggle('header-mobile-nav_show');
     });
